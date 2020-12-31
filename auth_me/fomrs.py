8a300da3f16ede68978756d143f10a53baa7eeb3
spoxlax <amer.assouli@gmail.com>
2020-12-31 19:21:45 +0100

    Password validation check

--- auth_me/fomrs.py
+++ auth_me/fomrs.py
@@ -1,12 +1,34 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class SignupForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "id": "inlineFormInputGroup"}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control", "id": "inputEmail3"}))
+    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "id": "inlineFormInputGroup",
+                                                             "placeholder": "John Doe"}))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={"class": "form-control", "id": "inputEmail3", "placeholder": "exemple@email.com"}))
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={"class": "form-control", "id": "exampleInputPassword1"}))
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "id": "exampleInputPassword1", "placeholder": " your secret password"}))
     password_validation = forms.CharField(
-        widget=forms.PasswordInput(attrs={"class": "form-control", "id": "exampleInputPassword1"}))
-    ext_check = forms.BooleanField(
-        widget=forms.CheckboxInput(attrs={"class": "form-check", "id": "gridCheck1", "label": " yes i am"}))
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "id": "exampleInputPassword1", "placeholder": " Confirm your password"}))
+    Are_you_an_external_agent = forms.BooleanField(required=False,
+                                                   widget=forms.CheckboxInput(
+                                                       attrs={"class": "form-check", "id": "gridCheck1",
+                                                              "label": " yes i am"}))
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        password_val = self.cleaned_data.get('password_validation')
+        if password!= password_val:
+            raise forms.ValidationError(" password mismatch")
+        return password
+
+    # def clean_username(self):
+    #     agent = self.cleaned_data.get('username')
+    #     qs = User.objects.filter(username=agent)
+    #     print(qs)
+    #     if qs:
+    #         raise forms.ValidationError("user is already in data base")
+    #     return agent
--- auth_me/views.py
+++ auth_me/views.py
@@ -7,8 +7,16 @@ def login_page(request):
 
 
 def signup_page(request):
-    signup_form = SignupForm()
+    signup_form = SignupForm(request.POST)
     context = {
-        "form": signup_form
+        "form": signup_form,
     }
-    return render(request, 'auth_me/signup.html', context)
+    if request.method == "GET":
+        return render(request, 'auth_me/signup.html', context)
+
+    elif request.method == "POST":
+        if signup_form.is_valid():
+            return render(request, 'auth_me/signup.html', context)
+    else:
+        print(" unvalid data")
+        return render(request, 'auth_me/signup.html', context)
